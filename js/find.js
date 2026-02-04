9a8f32452ac3bbfd2a8d44b31793101cea44163a
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-02-05 00:24:48 +0600

    Encapsulation in js

--- js/find.js
+++ js/find.js
@@ -1,5 +1,5 @@
 const  number = [21,32,41,,43,53,56,22]
 
 
-const result = number.find(num => num >= 60)
+const result = number.find(num => num > 60)
 console.log(result);
--- js/this.js
+++ js/this.js
@@ -1,8 +1,14 @@
 class Car {
+    #lock
     constructor (name, brand, price){
         this.name = name,
         this.brand = brand,
-        this.price = price
+        this.price = price,
+        this.#lock = "343reref45re"
+    }
+    getLock(){
+        console.log(this.#lock);
+        
     }
     getThis(){
         console.log(this.price);
@@ -14,8 +20,11 @@ const car2 = new Car("Bench", "Marchidis", 1200000)
 // car1.getThis()
 // console.log(car1);
 
-console.log(car2);
-car2.getThis()
+// console.log(car2);
+// car2.getThis()
+// console.log(car1);
+car1.getLock()
+
 
 
 const person = {
@@ -24,7 +33,12 @@ const person = {
     add :function (){
         console.log(this);
         
+    },
+    arrow : () =>{
+        console.log(this);
+        
     }
 }
-console.log(person);
-person.add()
+// console.log(person);
+// person.add()
+person.arrow()
