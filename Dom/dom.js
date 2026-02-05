fdea7d376ceadd89cce1210c9374459906009d9b
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-02-05 16:41:29 +0600

    append child

--- Dom/dom.js
+++ Dom/dom.js
@@ -1,6 +1,8 @@
 // document.getElementsByTagName("h2")[0].style.color = "red"
 // document.querySelector("h2").style.color= "red"
 
+// const { createElement } = require("react")
+
 const title = document.getElementsByTagName("h2")
 // title[0].style.textAlign = "center"
 // console.log(title);
@@ -22,11 +24,11 @@ place.style.textAlign = "center"
 
 const everyPlace = document.getElementsByClassName("everyPlace")
 
-console.log(everyPlace);
+// console.log(everyPlace);
 
 for (const place of everyPlace) {
     place.style.textAlign = "center";
-    console.log(place.innerText);
+    // console.log(place.innerText);
     
 }
 
@@ -53,12 +55,25 @@ const Title = document.getElementById("gadgets")
 Title.classList.remove("chang")
 Title.classList.add("bg-center")
 Title.setAttribute("href", "abusaleh.com")
-console.log(Title.getAttribute("class"));
-console.log(Title.classList);
+// console.log(Title.getAttribute("class"));
+// console.log(Title.classList);
 
 const tour = document.getElementById("place")
 // tour.innerHTML = "<h2> how tour place</h2>"
 tour.innerText = "good tour place"
 
+const Gadget = document.getElementById("gadget-container")
+// console.log(Gadget.childNodes[3].childNodes[3]);
 
-// 5
+const ul = document.querySelector("#gadget-container ul")
+// console.log(ul);
+
+const li1 = document.createElement("li")
+li1.innerText = "AirBirds"
+const li2 = document.createElement("li")
+li2.innerText = "Nickband"
+
+ul.appendChild(li1)
+ul.appendChild(li2)
+
+console.log(ul.parentNode.parentNode.parentNode);
