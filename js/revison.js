54592c5bbef1765e5acdfe71fb1c25999e1c29fa
abusaleh2234 <121687863+abusaleh2234@users.noreply.github.com>
2026-02-19 21:38:34 +0600

    practice destructuring

---
+++ js/revison.js
@@ -0,0 +1,16 @@
+const names = ["Rahim", "karim", "Salam", "Rafik","Jamal"]
+
+const [n1,n2,n3] = names
+
+// console.log(n1,n2,n3);
+
+const person = {
+    name: "Sakib",
+    age: 18,
+    class:12,
+    friends: ["Siam", "Joni"]
+}
+
+const {age, friends} = person;
+
+console.log(friends, age);
